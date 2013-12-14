171b7696d5c271e31d21c113803dc5e97eb607d2
dayueqq <2252990046@qq.com>
2013-12-14 20:46:46 +0800

    2013.12.14
    1.add custom scrollbar for left-ul-box
    2.solved the getElementsByClassName compacity in IE explorer

--- res/js/newtest.js
+++ res/js/newtest.js
@@ -1,4 +1,22 @@
 // JavaScript Document
+//解决getElementById在IE下失效的函数 
+ function getElementsByClassName(classname){  
+                    var d=document;  
+                    var e=d.getElementsByTagName('*');  
+                    var c=new RegExp('\\b'+classname+'\\b');  
+                    var r=[];  
+                      
+                    for(var i=0,l=e.length;i<l;i++){  
+                        var cn=e[i].className;  
+                        if(c.test(cn)){  
+                            r.push(e[i]);  
+                        }  
+                    }  
+                    return r;  
+     }  
+    if(typeof document.getElementsByClassName !='function'){  
+         document.getElementsByClassName=getElementsByClassName;  
+    }  
 //图片透明蒙版
  function openVessel(strId)
     {
